fc9462fa5e4cbf04c8db5ce959e41f0f2f646b83
Donald Dixon <dixondonald@gmail.com>
2016-01-28 14:53:54 -0500

    Checkpoint 23 completed

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Ziggy on 1/28/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Ziggy on 1/28/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
